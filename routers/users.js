450931af7671be77557f1a4f9de98f6670f9d05e
Stanislav Galynia <strider9494@gmail.com>
2018-12-16 12:41:41 +0300

    Add web token

--- routers/users.js
+++ routers/users.js
@@ -1,31 +1,56 @@
 const express = require('express');
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
-const User = require ('../database')
+const jwt = require('jsonwebtoken');
+const User = require ('../database');
+
 
 const router = express.Router();
 const salt = bcrypt.genSaltSync(10);
 
 router.use(express.json());
 
+// verifyToken = (req, res, next) => {
+//   const bearer 
+// };
+
 
 router.post('/log', async (req, res) => {
   const schema = {
-    name: Joi.string().min(3).required(),
-    password: Joi.string().min(3).required()
+    name: Joi.string().min(4).required(),
+    password: Joi.string().min(4).required()
   }
 
   const result = Joi.validate(req.body, schema);
 
   if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+    res.status(502).send(result.error.details[0].message);
     return
   }
 
   const user = await User.findOne({ name: req.body.name});
 
-  if(!user || !bcrypt.compareSync(req.body.password, user.password)) return res.status(400).send({log:false, reason:'Invalid login or password'})
-  res.json({log: true});
+  if(user && bcrypt.compareSync(req.body.password, user.password)) {
+    const userParams = {
+      name: user.name,
+      email: user.email,
+      id: user._id
+    };
+  
+    jwt.sign ({userParams}, 'seсretkey', {expiresIn: '24h'}, (err, token) =>{
+      res.json({
+        log: true,
+        user: user,
+        token
+        });
+    });
+
+  } else {
+    return res.status(403).json({ log:false, reason:'Invalid login or password' });
+  }
+
+  
+  
 });
 
 router.post('/reg', async (req, res) => {
